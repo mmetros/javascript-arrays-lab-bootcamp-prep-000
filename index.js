6ee996585bbb5a1a59cb71ed69c2db39eb87b89c
mmetros@wesleyan.edu <mmetros@wesleyan.edu>
2018-07-30 05:20:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your functions and code here
+
+
+// var kittens = ["Jeff", "Matt"];
+// console.log(kittens.concat("Tuna"));
+// console.log(kittens.slice(0,1))
+// console.log(kittens.splice(0,0,"Howard"))
+// console.log(kittens);
+
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  
+  return kittens.push(name)
+}
+
+kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+kittens = ["Milo", "Otis", "Garfield"];
+
+destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+kittens = ["Milo", "Otis", "Garfield"];
+
+
+
+
+
+
+
+
+
+
+
+
